11732e39268c17cad1c93e152b634c5cae249989
n-rdy <giuseppegelardi@icloud.com>
2021-10-01 19:47:48 -0500

    app basically done, need to add some styles and redirect back to index after delete

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'myapp'
@@ -8,7 +10,6 @@ urlpatterns = [
     path('new/', views.create, name='create_new_superhero'),
     path('edit/<superhero_id>/', views.edit, name='edit_existing_superhero'),
     path('delete/<superhero_id>/', views.delete, name='delete_existing_superhero')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-    # path('edit/<int:superhero_id>/', views.edit, name='edit_existing_superhero'),
-    # path('delete/<int:superhero_id>/', views.delete, name='delete_existing_superhero')
+
